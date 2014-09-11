9084f94120fa73913f91fb9325a8859c06b7c175
cdts_change <cdts_change@live.cn>
2014-09-11 23:03:29 +0800

    Add Learning file
    mutl thread

---
+++ Learning/mtSleep.py
@@ -0,0 +1,29 @@
+from time import sleep, ctime
+from myThread import MyThread
+
+loops = (4,2)
+
+def loop(nloop,nsec):
+    print 'start loop', nloop, 'at:', ctime()
+    sleep(nsec)
+    print 'loop', nloop, 'done at:', ctime()
+    
+def main():
+    print 'starting at:', ctime()
+    threads = []
+    nloops = range(len(loops))
+    
+    for i in nloops:
+        t = MyThread(loop, (i, loops[i]), loop.__name__)
+        threads.append(t)
+        
+    for i in nloops:
+        threads[i].start()
+        
+    for i in nloops:
+        threads[i].join()
+        
+    print 'all DONE at:', ctime()
+    
+if __name__ == '__main__':
+    main()
