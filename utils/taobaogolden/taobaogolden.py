4c688721c69739f4be47b7c5702d04b3ae51d3ef
cdts_change <cdts_change@live.cn>
2014-10-05 15:28:54 +0800

    modify taobao golden
    referrence :
    http://my.oschina.net/u/811744/blog/192604

--- utils/taobaogolden/taobaogolden.py
+++ utils/taobaogolden/taobaogolden.py
@@ -1,4 +1,6 @@
 #coding=utf-8
+
+#参考http://my.oschina.net/u/811744/blog/192604
 import HTMLParser  
 import urlparse  
 import urllib  
@@ -71,26 +73,37 @@ postData = {
 def loginToTaobao():
     #cookie 自动处理器
     global checkCodeUrl
-    cookiejar = cookielib.LWPCookieJar()#LWPCookieJar提供可读写操作的cookie文件,存储cookie对象
-    cookieSupport= urllib2.HTTPCookieProcessor(cookiejar)
+    global token
+    cookieJar = cookielib.LWPCookieJar()#LWPCookieJar提供可读写操作的cookie文件,存储cookie对象
+    cookieSupport= urllib2.HTTPCookieProcessor(cookieJar)
     opener = urllib2.build_opener(cookieSupport, urllib2.HTTPHandler)
     urllib2.install_opener(opener)
     #打开登陆页面
     taobao = urllib2.urlopen(tbLoginUrl)
     resp = taobao.read().decode("gbk")
-    #提取验证码地址
-    pattern = r'img id="J_StandardCode_m" src="https://s.tbcdn.cn/apps/login/static/img/blank.gif" data-src="(\S*)"'
-    checkCodeUrlList = re.findall(pattern, resp)
-    checkCodeUrl = checkCodeUrlList[0]
-    print "checkCodeUrl:", checkCodeUrl
+ 
+    displayCookies(cookieJar)
     #此时直接发送post数据包登录
-    while True:
-        result = sendPostData(tbLoginUrl, postData, headers)
-        if result:
-            break;
-        if checkCodeUrl != "":
-            getCheckCode(checkCodeUrl)
+    result = sendPostData(tbLoginUrl, postData, headers)#此时默认不需要输入验证码    
+    print "result: ", result
      
+    while(not result["state"]):
+        print "failed to login in, error message: ",result["message"]
+        if result["code"] == "3425" or result["code"] == "1000":             
+            checkCodeUrl = result["ccurl"]    
+            print "checkCodeUrl:", checkCodeUrl
+            getCheckCode(checkCodeUrl)
+        result=sendPostData(tbLoginUrl, postData, headers)
+        print "result: ", result
+    token = result["token"]
+    print "token:", token
+    print "successfully login in!"
+    
+def displayCookies(cookiejar):
+    print "+"*20+"displayCookies"+"+"*20
+    for cookie in cookiejar:
+        print cookie
+    
 def sendPostData(url, data, header):
     print "+"*20+"sendPostData"+"+"*20
     data = urllib.urlencode(data)      
@@ -105,51 +118,45 @@ def sendPostData(url, data, header):
     print info
     print "Response:", text
     result = handleResponseText(text)
-    if result["state"]:
-        print "successfully login in!"
-        return True
-    else:
-        print "failed to login in, error message: ",result["message"]
-        return False
+    return result
  
 def handleResponseText(text):
-    global token
-    """处理登录返回结果"""
-    global checkCodeUrl
-     
+    """处理登录返回结果"""   
     print "+"*20+"handleResponseText"+"+"*20  
     text = text.replace(',', ' ')   
     responseData = {"state": False,
                     "message" : "",
-                    "code" : ""}
+                    "code" : "",
+                    "token" : ""}
      
     m1 = re.match(r'\{?"state":(\w*)\ ', text)
     if m1 is not None:
         s = m1.group(1)
         if s == "true":
             responseData["state"] = True
-            m2 = re.search(r'"token":"(\S*)"( |})', text)
-            if m2 is not None:
-                token = m2.group(1)
-                print token
-            else:
-                print "failed to get the token"
+            #提取token
+            m4 = re.search(r'"token":"(\w*)"( |})', text)
+            if m4 is not None:
+                responseData["token"] = m4.group(1) 
         else:
             m2 = re.search(r'"message":"(\S*)"( |})', text)
             if m2 is not None:
                 msg = m2.group(1)
-                responseData["message"] = msg 
+                responseData["message"] = msg  
             else:
                 print "failed to get the error message"
-            m3 = re.search(r'"ccurl":"(\S*)"( |})', text)
+            m3 = re.match(r'.+\"code":(\w*)\ ', text)
             if m3 is not None:
-                ccurl = m3.group(1)
-                print ccurl
-                checkCodeUrl = ccurl
-#                 if code == "1000":                 
-#                     getCheckCode(checkCodeUrl)
+                code = m3.group(1)
+                responseData["code"] = code
             else:
                 print "failed to get the error code"
+            m4 = re.search(r'"ccurl":"(\S*)"( |})', text)
+            if m4 is not None:
+                ccurl = m4.group(1)
+                responseData["ccurl"] = ccurl  
+            else:
+                print "failed to get the error message"
     return responseData
  
 def getCheckCode(url):
@@ -158,7 +165,7 @@ def getCheckCode(url):
     status = response.getcode()
     picData = response.read()
      
-    path = "G:\\\mTool\\taobaogolden\\checkcode.jpg"
+    path = "G:\\mTool\\taobaogolden\\checkcode.jpg"
     if status == 200:
         localPic = open(path, "wb")
         localPic.write(picData)
@@ -169,10 +176,10 @@ def getCheckCode(url):
         postData["TPL_checkcode"] = checkCode
         postData["need_check_code"] = "true"
     else:
-        print "failed to get Check Code, status: ",status
- 
+        print "failed to get Check Code, status:",status
+        
+
 def getGolden():
-    
     global token
     queryData = {
              '_tb_token_':token
