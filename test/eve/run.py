7e1e834c8f9567a6e5fa34570b035786c1e5a765
cdts_change <cdts_change@live.cn>
2014-10-17 19:39:32 +0800

    add test curl

--- test/eve/run.py
+++ test/eve/run.py
@@ -3,3 +3,9 @@ app = Eve()
 
 if __name__ == '__main__':
 	app.run()
+
+#$ curl -i http://eve-demo.herokuapp.com/people?where={"lastname": "Doe"}
+#$ curl -i http://eve-demo.herokuapp.com/people?where=lastname=="Doe"
+#$ curl -i http://eve-demo.herokuapp.com/people?sort=city,-lastname
+#$ curl -i http://eve-demo.herokuapp.com/people?sort=[("lastname", -1)]
+#$ curl -i http://eve-demo.herokuapp.com/people?max_results=20&page=2
