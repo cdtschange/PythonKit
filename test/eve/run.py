ebc71f99b9369e030140333ab0fa23f4e380f3ed
cdts_change <cdts_change@live.cn>
2014-10-18 20:18:03 +0800

    add test

--- test/eve/run.py
+++ test/eve/run.py
@@ -4,8 +4,14 @@ app = Eve()
 if __name__ == '__main__':
 	app.run()
 
-#$ curl -i http://eve-demo.herokuapp.com/people?where={"lastname": "Doe"}
-#$ curl -i http://eve-demo.herokuapp.com/people?where=lastname=="Doe"
-#$ curl -i http://eve-demo.herokuapp.com/people?sort=city,-lastname
-#$ curl -i http://eve-demo.herokuapp.com/people?sort=[("lastname", -1)]
-#$ curl -i http://eve-demo.herokuapp.com/people?max_results=20&page=2
+#$ curl -i http://127.0.0.1:5000
+#$ curl http://127.0.0.1:5000/people
+#$ curl -d '[{"firstname": "barack", "lastname": "obama"}, {"firstname": "mitt", "lastname": "romney"}]' -H 'Content-Type: application/json'  http://127.0.0.1:5000/people
+#$ curl -i http://127.0.0.1:5000/people/obama
+
+#$ curl -i http://127.0.0.1:5000/people?where={"lastname": "Doe"}
+#$ curl -i http://127.0.0.1:5000/people?where=lastname=="Doe"
+#$ curl -i http://127.0.0.1:5000/people?sort=city,-lastname
+#$ curl -i http://127.0.0.1:5000/people?sort=[("lastname", -1)]
+#$ curl -i http://127.0.0.1:5000/people?max_results=20&page=
+#$ curl -H "Accept: application/xml" -i http://127.0.0.1:5000/people
