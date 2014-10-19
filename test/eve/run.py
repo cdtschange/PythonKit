c9af8a16602281e8d31dfe96d17325abf501652d
cdts_change <cdts_change@live.cn>
2014-10-19 21:27:31 +0800

    add eve test

--- test/eve/run.py
+++ test/eve/run.py
@@ -15,3 +15,10 @@ if __name__ == '__main__':
 #$ curl -i http://127.0.0.1:5000/people?sort=[("lastname", -1)]
 #$ curl -i http://127.0.0.1:5000/people?max_results=20&page=
 #$ curl -H "Accept: application/xml" -i http://127.0.0.1:5000/people
+#$ curl -H "If-Modified-Since: Wed, 05 Dec 2012 09:53:07 GMT" -i http://127.0.0.1:5000/people
+#$ curl -H "If-None-Match: 1234567890123456789012345678901234567890" -i http://127.0.0.1:5000/people
+#$ curl -X PATCH -i http://127.0.0.1:5000/people/544250040041e0087684e477 -d '{"firstname": "ronald"}'
+#$ curl -H "If-Match: 1234567890123456789012345678901234567890" -X PATCH -i http://127.0.0.1:5000/people/544250040041e0087684e477 -d '{"firstname": "ronald"}'
+#$ curl -H "If-Match: b14a4cd77b0a6f83f00617843aec71b890b01475" -X PATCH -i http://127.0.0.1:5000/people/544250040041e0087684e477 -d '{"firstname": "ronald"}'
+#$ curl -d '{"firstname": "barack", "lastname": "obama"}' -H 'Content-Type: application/json' http://127.0.0.1:5000/people
+#$ curl -d '[{"firstname": "barack", "lastname": "obama"}, {"firstname": "mitt", "lastname": "romney"}]' -H 'Content-Type: application/json' http://127.0.0.1:5000/people
